4692eb21340b2fb41b4f707f60523ed5517739e8
Llito <101676338+LlitoEly@users.noreply.github.com>
2023-01-06 13:15:56 -0500

    Add files via upload
    Descuento coche

---
+++ coche/js/vehiculo.js
@@ -0,0 +1,17 @@
+
+let car1, car2, descuento1,descuento2;
+
+car1 = window.prompt("INGRESE MARCA VEHICULO ");
+car2 = window.prompt("INGRESE MODELO VEHICULO ");
+
+
+if ((car1=="ford") && (car2 =="fiesta")) {
+  descuento1 = 5; 
+  alert("POR ESTE VEHICULO USTED TIENE UN DESCUENTO " + descuento1+ "%" );
+}
+  else { 
+  ((car1=="ford") && (car2 =="focus"))
+   descuento2 = 10;
+  }
+   alert("POR ESTE VEHICULO USTED TIENE UN DESCUENTO " +descuento2+ "%");
+  
